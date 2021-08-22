90c3927a1f7355ecd493e6513169c1a872379597
Axetroy <axetroy.dev@gmail.com>
2021-08-23 00:38:28 +0800

    update test

--- test.ts
+++ test.ts
@@ -3,27 +3,33 @@ import { assertEquals } from "https://deno.land/std@0.105.0/testing/asserts.ts";
 import { generate } from "./swagger2ts.ts";
 import { URL2filepath } from "./helper.ts";
 
-Deno.test({
-  name: "Generate",
-  fn: async () => {
-    async function testDir(dirName: string) {
-      const testDir = URL2filepath(new URL(`./__test__/${dirName}`, import.meta.url));
+async function testDir(dirName: string) {
+  const testDir = URL2filepath(new URL(`./__test__/${dirName}`, import.meta.url));
 
-      console.log('testDir: ', testDir)
+  console.log("testDir: ", testDir);
 
-      for await (const dirEntry of Deno.readDir(testDir)) {
-        if (/\.json$/.test(dirEntry.name)) {
-          console.log('path.join(testDir, dirEntry.name): ', path.join(testDir, dirEntry.name))
-          const actual = await generate(path.join(testDir, dirEntry.name));
+  for await (const dirEntry of Deno.readDir(testDir)) {
+    if (/\.json$/.test(dirEntry.name)) {
+      console.log("path.join(testDir, dirEntry.name): ", path.join(testDir, dirEntry.name));
+      const actual = await generate(path.join(testDir, dirEntry.name));
 
-          const expect = await Deno.readTextFile(path.join(testDir, dirEntry.name.replace(/\.json$/, ".ts")));
+      const expect = await Deno.readTextFile(path.join(testDir, dirEntry.name.replace(/\.json$/, ".ts")));
 
-          assertEquals(actual.trim(), expect.trim());
-        }
-      }
+      assertEquals(actual.trim(), expect.trim());
     }
+  }
+}
 
+Deno.test({
+  name: "Generate for swagger 3.0",
+  fn: async () => {
     await testDir("3.0");
+  },
+});
+
+Deno.test({
+  name: "Generate for swagger 3.1",
+  fn: async () => {
     await testDir("3.1");
   },
 });
