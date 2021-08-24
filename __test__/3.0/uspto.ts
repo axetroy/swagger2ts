f5964437349d4c932b174702671d6b848acf7c39
Axetroy <axetroy.dev@gmail.com>
2021-08-24 18:08:36 +0800

    v0.2.18

--- __test__/3.0/uspto.ts
+++ __test__/3.0/uspto.ts
@@ -307,7 +307,13 @@ export class Runtime implements IRuntime {
       const exec = () =>
         fetch(url.toString(), {
           method: config.method,
-          body: config.body instanceof RuntimeForm ? config.body.formData() : JSON.stringify(config.body),
+          body:
+            // disable body for methods
+            config.method === "GET" || config.method === "HEAD"
+              ? undefined
+              : config.body instanceof RuntimeForm
+              ? config.body.formData()
+              : JSON.stringify(config.body),
           headers: headers,
 
           // common options
